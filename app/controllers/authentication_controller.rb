e723c7a8f825a0653cf978ee6fc7867992318cd7
jordan-storz <jordan.storz.1@gmail.com>
2017-03-12 12:28:02 -0600

    add logging

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,16 +2,20 @@ class AuthenticationController < ApplicationController
   def github
     client_id = ENV["GITHUB_CLIENT"]
     client_secret = ENV["GITHUB_CLIENT_SECRET"]
+    puts "GITHUB INFO ..."
+    puts client_id
+    puts client_secret
+    puts "_____"
     code = params[:code]
     @result = HTTParty.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
     @access_token = @result.parsed_response.split('&')[0].split('=')[1]
     @user_info = HTTParty.get("https://api.github.com/user?access_token=#{@access_token}")
     user = find_or_create_user(@user_info["login"])
-    
+
     puts "RESPONSE FROM GITHUB API FOR USER"
     puts @user_info
     response = {username: @user_info["login"], token: @access_token}
-    render json: response 
+    render json: response
   end
 
   def find_or_create_user(username)
