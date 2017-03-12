793fa1ae8a47a62527b3adf0bb02153ffb5bdf0f
jordan-storz <jordan.storz.1@gmail.com>
2017-03-11 19:31:44 -0700

    Library models has Tags relationship (through Book)

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,4 +1,5 @@
 class Library < ApplicationRecord
   belongs_to :user, autosave: true
   has_and_belongs_to_many :books
+  has_many :tags, through: :books
 end
--- app/serializers/library_serializer.rb
+++ app/serializers/library_serializer.rb
@@ -2,4 +2,5 @@ class LibrarySerializer < ActiveModel::Serializer
   attributes :id, :user_id
   belongs_to :user
   has_many :books
+  has_many :tags, through: :books
 end
